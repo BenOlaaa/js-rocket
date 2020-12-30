e17a2d0f777d5e720f9e5f68682abb71e857add0
BenOlaaa <53540261+BenOlaaa@users.noreply.github.com>
2020-12-29 21:48:40 -0500

    js explanations

---
+++ explanations.js
@@ -0,0 +1,26 @@
+var benObject = {
+	firstName: 'Ben',
+	lastName: 'Olayiwola',
+	age: '29 years',
+	favColors: ['blue', 'grey', 'black'],
+	isMale: true;
+}
+
+
+var dosomething = function () {
+	console.log ('do something!!!')
+}
+// dosomething
+// dosomething ()
+
+
+var whatIsMyFavcolor = function (){
+	return 'blue';
+}
+// whatIsMyFavcolor()
+
+
+var fullname = function (firstName, lastName) {
+	return firstName + lastName;
+}
+// fullname('Ben', 'Ola')
