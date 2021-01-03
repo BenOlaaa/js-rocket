83a7d560c8e586a2d55fb36716ac94473be8d9ec
BenOlaaa <53540261+BenOlaaa@users.noreply.github.com>
2021-01-03 12:09:23 -0500

    added cs and js

--- explanations.js
+++ explanations.js
@@ -1,34 +1,64 @@
-var benObject = {
-	firstName: 'Ben',
-	lastName: 'Olayiwola',
-	age: '29 years',
-	favColors: ['blue', 'grey', 'black'],
-	isMale: true;
-}
+// var benObject = {
+// 	firstName: 'Ben',
+// 	lastName: 'Olayiwola',
+// 	age: '29 years',
+// 	favColors: ['blue', 'grey', 'black'],
+// 	isMale: true;
+// }
 
 
-var dosomething = function () {
-	console.log ('do something!!!')
-}
-// dosomething
-// dosomething ()
+// var dosomething = function () {
+// 	console.log ('do something!!!')
+// }
+// // dosomething
+// // dosomething ()
 
 
 
-// FUNCTIONS
-var whatIsMyFavcolor = function (){
-	return 'blue';
-}
-// whatIsMyFavcolor()
+// // FUNCTIONS
+// var whatIsMyFavcolor = function (){
+// 	return 'blue';
+// }
+// // whatIsMyFavcolor()
 
 
-var fullname = function (firstName, lastName) {
-	return firstName + lastName;
-}
-// fullname('Ben','Ola')
+// var fullname = function (firstName, lastName) {
+// 	return firstName + lastName;
+// }
+// // fullname('Ben','Ola')
 
 
-// IF STATEMENT
-if (true) {
-	alert ('male');
-}
+// // IF STATEMENT
+// if (true) {
+// 	alert ('male');
+// }
+
+// -----------------------------------
+
+console.log ("My friends are Ballers!");
+		var car = {
+			make: 'VW',
+			type: 'Polo',
+			color: 'Blue',
+			isTurnedOn: false,
+			numberOfwheels: 4,
+			seats: ['seat1', 'seat2', 'seat3', 'seat4'],
+			turnedOn: function (){
+			this.isTurnedOn = true;
+			},
+			fly: function (){
+				alert('fly!!!!!!!!!!!!!!!!!!!!!!!');
+			},
+			switchCar: function (isOn){
+				console.log ('turn car is on?', '' + isOn);
+				if (isOn == true) {
+					this.isTurnedOn = true;
+				} else {
+					this.isTurnedOn = false;
+				}
+			}
+		};
+
+
+
+console.log ('Where is the car?')
